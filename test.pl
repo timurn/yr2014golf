9e261d9cb7e7bd732e1470c175411f82f0d5d1da
Timur Nozadze <timur.nozadze@gmail.com>
2014-06-14 09:24:13 +0400

    Ещё тесты

--- test.pl
+++ test.pl
@@ -56,6 +56,39 @@ Mendeleev, Ivanov: How to make vodka
 ',
         out => "Inf\n"
     },
+    {
+        in => 
+'Erdos
+Smith, Martin, Erdos: Newtonian forms of prime factor matrices 
+Erdos, Reisig: Stuttering in petri nets
+Smith, Chen: First oder derivates in structured programming
+Jablonski, Hsueh: Selfstabilizing data structures
+Erdos: Erdos number and its applications
+',
+        out => "0\n"
+    },
+    {
+        in => 
+'Erdos
+Smith, Martin, Erdos: Newtonian forms of prime factor matrices 
+Erdos, Reisig: Stuttering in petri nets
+Smith, Chen: First oder derivates in structured programming
+Jablonski, Hsueh: Selfstabilizing data structures
+Erdos, Erdos: Multiple personality disorder
+',
+        out => "0\n"
+    },
+    {
+        in => 
+'Chen
+Smith, Martin, Erdos: Newtonian forms of prime factor matrices 
+Erdos, Reisig: Stuttering in petri nets
+Smith, Chen: First oder derivates in structured programming
+Jablonski, Hsueh: Selfstabilizing data structures
+Erdos, Chen: Close connections
+',
+        out => "1\n"
+    },
 );
 
 my ( $in, $out, $err );
